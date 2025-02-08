ed75956b5662c016a0628b87c7d11f690dc5b77f
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-02-08 22:08:51 +0500

    add MongoDB connection setup and update environment variables

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { PORT } from "./config/env.js";
 import authRouter from "./routes/authRoute.js";
+import connectDB from "./database/mongodb.js";
 
 const app = express();
 
@@ -12,4 +13,6 @@ app.use("/api/v1/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+
+  connectDB();
 });
