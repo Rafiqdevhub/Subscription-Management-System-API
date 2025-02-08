297e02ddb1247a9d1b4ffe2bcaf20865693ecf1f
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-02-08 21:22:11 +0500

    add environment configuration and update server to use dynamic port

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from "express";
+import { PORT } from "./config/env.js";
 
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
