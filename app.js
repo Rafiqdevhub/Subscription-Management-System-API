ee6a824394319ac0030b129ad73387af03cdfc73
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-02-08 21:45:31 +0500

    add user authentication routes and user model with controller functions

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import authRouter from "./routes/authRoute.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api/v1/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
---
+++ controllers/authController.js
@@ -0,0 +1,5 @@
+const signUp = async (req, res) => {};
+const signIn = async (req, res) => {};
+const signOut = async (req, res) => {};
+
+export { signUp, signIn, signOut };
