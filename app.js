8431a47a288159295906947c2f612eca4eb2601b
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-02-09 20:01:20 +0500

    add error handling middleware and configure app to use cookie parser

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ import connectDB from "./database/mongodb.js";
 import userRouter from "./routes/userRoute.js";
 import subscriptionRouter from "./routes/subscriptionRoute.js";
 import workflowRouter from "./routes/workflowRoute.js";
+import errorMiddleware from "./middlewares/errorMiddleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -16,6 +22,8 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/workflows", workflowRouter);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   connectDB();
