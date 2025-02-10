05eb77c2248d79b35ddeb8e64a73a280c272d2f9
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-02-10 20:49:22 +0500

    add Arcjet middleware for bot detection and rate limiting

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ import subscriptionRouter from "./routes/subscriptionRoute.js";
 import workflowRouter from "./routes/workflowRoute.js";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
 import cookieParser from "cookie-parser";
+import arcjetMiddleware from "./middlewares/arcjetMiddleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
