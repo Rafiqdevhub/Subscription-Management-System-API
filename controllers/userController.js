5ad86ece0d6869d9c008f6957bf69ea5d7c2a630
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-02-10 14:58:09 +0500

    implement user retrieval endpoints and add authorization middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,34 @@
-const getUsers = async (req, res) => {};
-const getUser = async (req, res) => {};
+import User from "../models/userModel.js";
+
+const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find().select("-password");
+
+    res.status(200).json({
+      success: true,
+      data: users,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id).select("-password");
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 const createUser = async (req, res) => {};
 const updateUser = async (req, res) => {};
 const deleteUser = async (req, res) => {};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,11 +6,12 @@ import {
   getUsers,
   updateUser,
 } from "../controllers/userController.js";
+import { authorize } from "../middlewares/authMiddleware.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
-userRouter.get("/:id", getUser);
+userRouter.get("/:id", authorize, getUser);
 userRouter.post("/", createUser);
 userRouter.put("/:id", updateUser);
 userRouter.delete("/:id", deleteUser);
